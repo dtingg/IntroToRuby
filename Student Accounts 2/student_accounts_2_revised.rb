0a56856d18b722854dc7553cd58e88489faff830
Dianna Tingg <dtingg@uw.edu>
2018-09-11 18:26:58 -0700

    updated

--- Student Accounts 2/student_accounts_2_revised.rb
+++ Student Accounts 2/student_accounts_2_revised.rb
@@ -6,63 +6,74 @@ puts "Welcome to the Student Account Generator\n"
 puts "\n"
 
 # Create one array that holds hashes
-$student_data = []
+student_data = []
 
 # Function to create a hash for each student
-def student_entry(student_name)
+def student_entry(student_name, student_data)
   name = student_name
-  id = get_id
-  email = get_email(student_name, id)
+  id = get_id(student_data)
+  email = get_email(name, id)
 
-  hash = {name: name, id: id, email: email}
-  return hash
+  hash = { name: name, id: id, email: email }
 end
 
 # Function to get student id
-def get_id
-  test = 0
+def get_id(student_data)
+  unique = false
 
-  # Make sure id doesn't already exist
-  until test != 0 && !$student_data.include?(test)
+  until unique
     test = rand(111111..999999)
+    unique = check_id(test, student_data)
   end
   return test
+end
+
+# Function to check id
+def check_id(test, student_data)
+  if student_data.empty?
+    return true
+  end
+
+  for i in student_data
+    if i[:id] == test
+      return false
+    end
+  end
+  return true
 end
 
 # Function to get username
 def username(student_name)
   name_parts = student_name.split
-  username = ""
+  username = ''
 
   i = 0
-  while i != name_parts.length-1 do
-    username.concat(name_parts[i][0])
+  while i != name_parts.length - 1 do
+    username += name_parts[i][0]
     i += 1
   end
 
-  username.concat(name_parts[-1])
-  return username
+  username += name_parts[-1]
 end
 
 # Function to get email
 def get_email(student_name, id)
   email = username(student_name) + id.to_s.slice(-3..-1) + "@adadevelopersacademy.org"
-  return email
 end
 
 # Ask user to enter a student name
 loop do
   print "Enter a student's first and last name for type Q to quit: "
   student = gets.chomp.upcase
-  if student == "Q"
+  if student == 'Q'
     break
   end
-  $student_data << student_entry(student)
+  student_data << student_entry(student, student_data)
 end
 
 # Print out all the student names, id numbers, and e-mail addresses in parallel
 puts "\nStudent Roster:"
 
-for h in $student_data
+for h in student_data
   puts "Student: #{h[:name]}, ID: #{h[:id]}, E-mail: #{h[:email]}"
 end
